ec3a4d4889c994bfa649cd0792a4dc7efb78011b
Noertri <trinuruns@gmail.com>
2024-12-04 21:54:30 +0700

    membuat kerangka ui

--- main.py
+++ main.py
@@ -2,10 +2,37 @@ import tkinter as tk
 from tkinter import ttk
 import os
 from pathlib import Path
-from collections import OrderedDict
 
 
-env_vars = dict(os.environ)
+class App(tk.Tk):
 
-for name, values in env_vars.items():
-    print(f"{name}={values.strip()}")
+    def __init__(self):
+        super().__init__()
+        self.title("Environment Variables")
+        # self.geometry("500x500")
+
+        self.label_frame1 = ttk.LabelFrame(self, text="User Variables")
+        self.label_frame1.grid(column=0, row=0, padx=5, pady=5)
+
+        self.frame1 = ttk.Frame(self.label_frame1, padding=(5, 5, 5, 5))
+        self.frame1.grid(column=0, row=0)
+
+        self.frame2 = ttk.Frame(self.label_frame1, padding=(5, 5, 5, 5))
+        self.frame2.grid(column=0, row=1, sticky="E")
+        
+        self.tree_view = ttk.Treeview(self.frame1, columns=["Name"])
+        self.tree_view.grid(column=0, row=0)
+
+        self.btn1 = ttk.Button(self.frame2, text="new")
+        self.btn1.grid(column=0, row=0)
+
+        self.btn2 = ttk.Button(self.frame2, text="edit")
+        self.btn2.grid(column=1, row=0, padx=(5, 5))
+
+        self.btn3 = ttk.Button(self.frame2, text="delete")
+        self.btn3.grid(column=2, row=0)
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
