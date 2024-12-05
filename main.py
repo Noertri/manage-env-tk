0fc40859d1dcd39357ca34b0d5eaa455e0ca1db3
Noertri <trinuruns@gmail.com>
2024-12-05 13:42:11 +0700

    mebuat callback untuk tombol new

--- main.py
+++ main.py
@@ -12,43 +12,57 @@ class Tabel(ttk.Treeview):
         super().__init__(parent, show="headings", *args, **kwargs)
         
         self.parent = parent
+        
         self.heading(0, text="Variable", anchor="center")
         self.heading(1, text="Values", anchor="center")
         self.column(0, width=200)
         self.column(1, width=300)
         self.bind("<Motion>", "break")
-        self.bind()
-
-        # frame tombol new, edit, dan delete
-        self.btn_frame = ttk.Frame(self.parent)
-        self.btn_frame.pack(pady=(10, 0), anchor="e", side="bottom")
-
-        # tombol new
-        self.btn_new = ttk.Button(self.btn_frame, text="New")
-        self.btn_new.pack(side="left")
-
-        # tombol edit
-        self.btn_edit = ttk.Button(self.btn_frame, text="Edit")
-        self.btn_edit.pack(padx=(5, 0), side="left")
-
-        # tombol delete
-        self.btn_del = ttk.Button(self.btn_frame, text="Delete")
-        self.btn_del.pack(padx=(5, 0), side="left")
 
         # memuat environment variables
         self.load_env_vars()
 
     def load_env_vars(self):
-        data = {
-            "ex_variables": [],
-            "common_variables": ["PATH"]
-        }
-
-        for item in dict(os.environ).items():
+        for item in sorted(dict(os.environ).items(), key=lambda x: x[0]):
             self.insert("", index=tk.END, values=item)
 
-    def selected_item(self, event=None):
-        pass
+
+class NewBtnWindow(tk.Toplevel):
+
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.parent = parent
+
+        self.title("New Variable")
+        self.resizable(False, False)
+
+        frame0 = ttk.Frame(self)
+        frame0.pack(anchor="center", padx=(20, 20), pady=(20, 20))
+
+        frame1 = ttk.Frame(frame0)
+        frame1.pack(anchor="center")
+
+        frame2 = ttk.Frame(frame0)
+        frame2.pack(anchor="center", pady=(10, 0))
+
+        frame3 = ttk.Frame(frame0)
+        frame3.pack(anchor=tk.E, pady=(10, 0))
+
+        label1 = ttk.Label(frame1, text="Variable", width=10)
+        label2 = ttk.Label(frame2, text="Value(s)", width=10)
+        entry1 = ttk.Entry(frame1, width=32)
+        entry2 = ttk.Entry(frame2, width=32)
+
+        label1.grid(column=0, row=0, sticky=tk.W)
+        entry1.grid(column=1, row=0, sticky=tk.W)
+        label2.grid(column=0, row=1, sticky=tk.W)
+        entry2.grid(column=1, row=1, sticky=tk.W)
+
+        btn_add = ttk.Button(frame3, text="Add")
+        btn_add.pack(side="left")
+
+        btn_cancel = ttk.Button(frame3, text="Cancel")
+        btn_cancel.pack(padx=(5, 0), side="left")
 
         
 class App(tk.Tk):
@@ -57,41 +71,60 @@ class App(tk.Tk):
         super().__init__()
         self.title("Environment Variables")
         # self.geometry("500x500")
+        self.resizable(False, False)
         
         main_style = ttk.Style()
-        main_style.configure("Treeview.Heading", font=tkfont.Font(weight="normal", size=10))
+        main_style.configure("Treeview.Heading", font=tkfont.Font(weight="normal"))
 
         # frame utama
-        self.main_frame = ttk.Frame(self)
-        self.main_frame.pack(anchor="center", padx=(20, 20), pady=(20, 20))
+        main_frame = ttk.Frame(self)
+        main_frame.pack(anchor="center", padx=(20, 20), pady=(20, 20))
         
         # label frame
-        self.label_frame1 = ttk.LabelFrame(self.main_frame, text="User Variables", padding=(10, 10, 10, 10))
-        self.label_frame1.pack(anchor="center")
+        label_frame1 = ttk.LabelFrame(main_frame, text="User Variables", padding=(10, 10, 10, 10))
+        label_frame1.pack(anchor="center")
 
         # tabel
-        self.tabel = Tabel(self.label_frame1, column=(0, 1), height=8)
+        self.tabel = Tabel(label_frame1, column=(0, 1), height=8)
         self.tabel.pack(anchor="center")
 
+        # frame tombol new, edit, dan delete
+        btn_frame = ttk.Frame(label_frame1)
+        btn_frame.pack(pady=(10, 0), anchor="e", side="bottom")
+
+        # tombol new
+        btn_new = ttk.Button(btn_frame, text="New", command=self.btn_new_callback)
+        btn_new.pack(side="left")
+
+        # tombol edit
+        btn_edit = ttk.Button(btn_frame, text="Edit")
+        btn_edit.pack(padx=(5, 0), side="left")
+
+        # tombol delete
+        btn_del = ttk.Button(btn_frame, text="Delete")
+        btn_del.pack(padx=(5, 0), side="left")
+
         # frame tombol konfirmasi
-        self.confirm_frame = ttk.Frame(self.main_frame)
-        self.confirm_frame.pack(anchor="e", pady=(20, 0))
+        confirm_frame = ttk.Frame(main_frame)
+        confirm_frame.pack(anchor="e", pady=(20, 0))
 
         # tombol ok
-        self.btn_ok = ttk.Button(self.confirm_frame, text="OK")
-        self.btn_ok.pack(side="left")
+        btn_ok = ttk.Button(confirm_frame, text="OK")
+        btn_ok.pack(side="left")
 
         # tombol cancel
-        self.btn_cancel = ttk.Button(self.confirm_frame, text="Cancel", command=self.btn_cancel_callback)
-        self.btn_cancel.pack(padx=(5, 0), side="left")
+        btn_cancel = ttk.Button(confirm_frame, text="Cancel", command=self.btn_cancel_callback)
+        btn_cancel.pack(padx=(5, 0), side="left")
 
-    def btn_cancel_callback(self, event=None):
+    def btn_cancel_callback(self):
         print("Program is closed...")
         self.destroy()
-        return -1
+
+    def btn_new_callback(self):
+        dialog_win = NewBtnWindow(self)
+        
 
 
 if __name__ == "__main__":
     app = App()
-    app.resizable(False, False)
     app.mainloop()
