92a65b58980911f97f626df01c57ada93d934ae2
Noertri <trinuruns@gmail.com>
2024-12-05 11:21:26 +0700

    memindahkan widget tombol new, edit, dan delete ke class Table

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 import tkinter.font as tkfont
 import os
 from pathlib import Path
+import pickle
 
 
 class Tabel(ttk.Treeview):
@@ -10,15 +11,39 @@ class Tabel(ttk.Treeview):
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, show="headings", *args, **kwargs)
         
+        self.parent = parent
         self.heading(0, text="Variable", anchor="center")
         self.heading(1, text="Values", anchor="center")
         self.column(0, width=200)
         self.column(1, width=300)
         self.bind("<Motion>", "break")
         self.bind()
-        self.load_envs()
 
-    def load_envs(self):
+        # frame tombol new, edit, dan delete
+        self.btn_frame = ttk.Frame(self.parent)
+        self.btn_frame.pack(pady=(10, 0), anchor="e", side="bottom")
+
+        # tombol new
+        self.btn_new = ttk.Button(self.btn_frame, text="New")
+        self.btn_new.pack(side="left")
+
+        # tombol edit
+        self.btn_edit = ttk.Button(self.btn_frame, text="Edit")
+        self.btn_edit.pack(padx=(5, 0), side="left")
+
+        # tombol delete
+        self.btn_del = ttk.Button(self.btn_frame, text="Delete")
+        self.btn_del.pack(padx=(5, 0), side="left")
+
+        # memuat environment variables
+        self.load_env_vars()
+
+    def load_env_vars(self):
+        data = {
+            "ex_variables": [],
+            "common_variables": ["PATH"]
+        }
+
         for item in dict(os.environ).items():
             self.insert("", index=tk.END, values=item)
 
@@ -47,22 +72,6 @@ class App(tk.Tk):
         # tabel
         self.tabel = Tabel(self.label_frame1, column=(0, 1), height=8)
         self.tabel.pack(anchor="center")
-
-        # frame tombol new, edit, dan delete
-        self.btn_frame = ttk.Frame(self.label_frame1)
-        self.btn_frame.pack(after=self.tabel, pady=(10, 0), anchor="e")
-
-        # tombol new
-        self.btn_new = ttk.Button(self.btn_frame, text="New")
-        self.btn_new.pack(side="left")
-
-        # tombol edit
-        self.btn_edit = ttk.Button(self.btn_frame, text="Edit")
-        self.btn_edit.pack(padx=(5, 0), side="left")
-
-        # tombol delete
-        self.btn_del = ttk.Button(self.btn_frame, text="Delete")
-        self.btn_del.pack(padx=(5, 0), side="left")
 
         # frame tombol konfirmasi
         self.confirm_frame = ttk.Frame(self.main_frame)
