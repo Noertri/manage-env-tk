513598b32e96512b63e3cc4dc075e5bd9a6a5f86
Noertri <trinuruns@gmail.com>
2024-12-07 10:57:41 +0700

    menambah fitur untuk menampilkan variabel yg dipilih

--- popup_windows.py
+++ popup_windows.py
@@ -68,12 +68,29 @@ class ListBoxPanel(ttk.Frame):
         super().__init__(parent, *args, **kwargs)
         self.parent = parent
         self.data = data
+        self.var_values = tk.StringVar(value=data[1].split(":"))
 
+        list_box_frame = ttk.Frame(self)
+        list_box_frame.pack(side="left")
+        
+        # bagian yang menampilkan nama variabel yang dipilih
+        var_box_frame = ttk.Frame(list_box_frame)
+        var_box_frame.grid(column=0, row=0, sticky=tk.NW)
 
+        label = ttk.Label(var_box_frame, text="Variable:")
+        label.grid(column=0, row=0, sticky=tk.NW)
 
-        list_box = tk.Listbox(self, width=30)
-        list_box.pack(side="left")
+        label1 = ttk.Label(var_box_frame, text=data[0])
+        label1.grid(column=1, row=0, sticky=tk.NW, padx=(10, 0))
 
+        # bagian yang menampilkan nilai variabel yang dipilih
+        label2 = ttk.Label(list_box_frame, text="Value(s):")
+        label2.grid(column=0, row=1, sticky=tk.NW, pady=(10, 0))
+
+        list_box = tk.Listbox(list_box_frame, width=40, listvariable=self.var_values, setgrid=True)
+        list_box.grid(column=0, row=2, sticky=tk.NW, pady=(10, 0))
+
+        # tombol
         btn_frame = ttk.Frame(self)
         btn_frame.pack(anchor="n", padx=(10, 0))
 
@@ -88,15 +105,13 @@ class EditBtnWindow(tk.Toplevel):
         self.parent = parent
         self.data = data
 
-        print(data)
-
         self.title("Edit Variable")
         # self.resizable(False, False)
 
         frame0 = ttk.Frame(self)
         frame0.pack(anchor="nw", padx=(20, 20), pady=(20, 20))
         
-        list_box_panel = ListBoxPanel(frame0)
+        list_box_panel = ListBoxPanel(frame0, data=self.data)
         list_box_panel.pack()
 
         btn_frame = ttk.Frame(frame0)
